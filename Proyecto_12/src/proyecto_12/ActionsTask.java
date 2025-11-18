be2ced589e053a82dee7539531b30702560943c1
Juan Avila <jmanuelavilaperez@gmail.com>
2025-11-17 19:00:52 -0500

    proyecto 12

---
+++ Proyecto_12/src/proyecto_12/ActionsTask.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_12;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Juan Avila
+ */
+public class ActionsTask {
+    
+    JPanel containerTask;
+    
+    public ActionsTask(JPanel containerTask)
+    {
+        this.containerTask = containerTask;
+    }
+    
+    public void createTask(String nameTask) 
+    {
+        JPanel newTask = new JPanel();        
+        JCheckBox newCheckBoxTask = new JCheckBox(nameTask);
+        //JTextField textField = new JTextField(nameTask);
+        JButton deleteButton = new JButton("edit"),
+                editButton = new JButton("del");
+        
+        newTask.setLayout(new FlowLayout(FlowLayout.LEFT));
+        
+        newCheckBoxTask.addActionListener(l -> {
+            changeState(newTask, !newCheckBoxTask.isSelected());});
+        deleteButton.addActionListener(e -> {deleteTask(newTask);});
+        editButton.addActionListener(e -> {editTask();});
+        
+        newTask.add(newCheckBoxTask);
+        //newTask.add(textField);
+        newTask.add(deleteButton);
+        newTask.add(editButton);
+
+        containerTask.add(newTask);
+        
+        containerTask.revalidate();
+        containerTask.repaint();
+    }
+    
+    void editTask()
+    {}
+    
+    void saveTask(JPanel task)
+    {}
+    
+    void changeState(JPanel task, boolean state)
+    {
+        task.setEnabled(state);
+        containerTask.revalidate();
+        containerTask.repaint();
+    }
+    
+    void deleteTask(JPanel task)
+    {
+        containerTask.remove(task);
+        containerTask.revalidate();
+        containerTask.repaint();
+    }
+    
+}
